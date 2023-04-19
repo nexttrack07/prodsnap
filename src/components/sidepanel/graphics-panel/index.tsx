e71f03bdc14e19d8838d060fdfee5b75695f691a
Faadhil Hakim <faadhil@nexttrack.biz>
2023-04-19 14:37:18 -0500

    modify SVGGraphic Type

--- src/components/sidepanel/graphics-panel/index.tsx
+++ src/components/sidepanel/graphics-panel/index.tsx
@@ -1,14 +1,8 @@
-import { SVGGraphicType } from '@/components/canvas/store';
 import { uuid } from '@/utils';
 import { Image, ScrollArea, SimpleGrid, Text } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-// import DATA from './data.json';
 import { getGraphics } from '@/api/template';
-
-// const getGraphics = (): Promise<SVGGraphicType[]> => {
-//   return Promise.resolve(DATA as unknown as SVGGraphicType[]);
-// };
 
 export function GraphicsPanel() {
   const query = useQuery(['graphics'], getGraphics);
@@ -26,15 +20,4 @@ export function GraphicsPanel() {
       </ScrollArea.Autosize>
     </div>
   );
-}
-
-function RenderGraphic({ graphic }: { graphic: SVGGraphicType }) {
-  const element = createSVGElement(graphic);
-  return <div>{element}</div>;
-}
-
-export function createSVGElement(graphic: SVGGraphicType): React.ReactElement {
-  const children = graphic.children.map(createSVGElement);
-
-  return React.createElement(graphic.name, graphic.attributes, graphic.value, ...children);
 }
