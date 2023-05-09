73db142332ce5dad073a37f9d12022f99754b6c6
Faadhil Hakim <faadhil@nexttrack.biz>
2023-05-09 14:06:42 -0500

    image upload to drf working

--- src/api/photos.ts
+++ src/api/photos.ts
@@ -23,10 +23,31 @@ type PhotoResponse = {
   images: Photo[];
 }
 
+type Image = {
+  id: number;
+  user: number;
+  image: string;
+}
+
+type ImageResponse = {
+  results: Image[];
+  next: string | null;
+  previous: string | null;
+  count: number;
+}
+
 export async function getPopularPhotos(page = 1) {
   return client.get<PhotoResponse>("/popular-photos?page=" + page).then(res => res.data);
 }
 
 export async function searchPhotos(query = 'bag', page = 1) {
   return client.get<PhotoResponse>("/search-photos?query=" + query + "&page=" + page).then(res => res.data);
+}
+
+export async function uploadPhoto(formData: FormData) {
+  return client.post<any>("/upload-image/", formData).then(res => res.data);
+}
+
+export async function getImages() {
+  return client.get<ImageResponse>("images").then(res => res.data);
 }
