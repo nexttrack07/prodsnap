f720de17937da850455c7ee039a93a7e240fd25d
Faadhil Hakim <faadhil@nexttrack.biz>
2023-05-17 17:18:57 -0500

    add rotate handler to multiple select

--- src/components/canvas/multiple-select.tsx
+++ src/components/canvas/multiple-select.tsx
@@ -1,7 +1,9 @@
-import { useAtom, useAtomValue } from 'jotai';
-import React from 'react';
+import { useAtom } from 'jotai';
 import { positionAtom, dimensionAtom } from '@/components/canvas/store';
 import { DragHandler } from './drag-handler';
+import { ResizeHandler } from './resize-handler';
+import { RotateHandler } from './rotate-handler';
+import { useState } from 'react';
 
 type Props = {
   show: boolean;
@@ -9,19 +11,26 @@ type Props = {
 
 export function MultipleSelect({ show }: Props) {
   const [position, setPosition] = useAtom(positionAtom);
-  const dimension = useAtomValue(dimensionAtom);
+  const [dimension, setDimension] = useAtom(dimensionAtom);
+  const [rotation, setRotation] = useState(0);
 
   if (!show) {
     return null;
   }
 
   return (
-    <DragHandler
-      withMoveHandle
-      withBorders
-      dimension={dimension}
-      position={position}
-      onMove={setPosition}
-    />
+    <>
+      <DragHandler
+        withMoveHandle
+        withBorders={false}
+        dimension={dimension}
+        position={position}
+        onMove={setPosition}
+        rotation={rotation}
+      >
+        <ResizeHandler dimension={dimension} onResize={setDimension} />
+        <RotateHandler dimension={dimension} position={position} onRotate={setRotation} />
+      </DragHandler>
+    </>
   );
 }
--- src/components/canvas/rotate-handler.tsx
+++ src/components/canvas/rotate-handler.tsx
@@ -9,28 +9,30 @@ type Props = {
   show?: boolean;
 };
 
-const useStyles = createStyles((theme) => ({
-  rotateHandler: {
-    position: 'absolute',
-    top: -40,
-    left: '50%',
-    width: 12,
-    height: 12,
-    cursor: 'grab',
-    transform: 'translate(-50%, -50%)',
-    border: `1px solid ${theme.colors.gray[5]}`,
-    borderRadius: '50%',
-    backgroundColor: theme.colors.gray[0],
-    '&:hover': {
-      filter: 'drop-shadow(0 0 3px rgba(0, 0, 0, 0.5))'
-    }
-  }
-}));
-
 export function RotateHandler({ dimension, position, onRotate, show = true }: Props) {
   const [isActive, setIsActive] = useState(false);
-  const [startAngle, setStartAngle] = useState(0);
   const [canvasPosition, setCanvasPosition] = useState({ x: 0, y: 0 });
+
+  const useStyles = createStyles((theme) => ({
+    rotateHandler: {
+      position: 'absolute',
+      // position it in the top, the distance should be min of -40px and max of -80px and by default
+      // it is 0.5 * width of the element
+      top: -Math.min(Math.max(40, 0.5 * dimension.width), 80),
+      left: '50%',
+      width: 12,
+      height: 12,
+      cursor: 'grab',
+      transform: 'translate(-50%, -50%)',
+      border: `1px solid ${theme.colors.gray[5]}`,
+      borderRadius: '50%',
+      backgroundColor: theme.colors.gray[0],
+      '&:hover': {
+        filter: 'drop-shadow(0 0 3px rgba(0, 0, 0, 0.5))'
+      }
+    }
+  }));
+
   const { classes } = useStyles();
   const center = useRef({ x: 0, y: 0 });
   const R2D = 180 / Math.PI;
@@ -45,29 +47,13 @@ export function RotateHandler({ dimension, position, onRotate, show = true }: Pr
 
   const handleRotateMouseDown = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
     e.stopPropagation();
-    const { clientX, clientY } = e;
 
     center.current = {
       x: position.x + dimension.width / 2,
       y: position.y + dimension.height / 2
     };
 
-    const posX = clientX - canvasPosition.x;
-    const posY = clientY - canvasPosition.y;
-    const x = posX - center.current.x;
-    const y = posY - center.current.y;
-
-    setStartAngle(R2D * Math.atan2(y, x));
     setIsActive(true);
-
-    // middle of the element
-    const middle = {
-      x: position.x + dimension.width / 2,
-      y: position.y - 40
-    };
-    console.log('expected position: ', middle);
-    console.log('actual position: ', { x: posX, y: posY });
-    console.log('actual position: ', { x: clientX, y: clientY });
   };
 
   useEffect(() => {
