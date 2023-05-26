ec583a866bd45b65303c6016ae3e3a224fb8f408
Faadhil Hakim <faadhil@nexttrack.biz>
2023-05-26 13:54:49 -0500

    update mousedown event

--- src/components/element-canvas/index.tsx
+++ src/components/element-canvas/index.tsx
@@ -6,22 +6,33 @@ import {
 import { Box } from '@mantine/core';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { ElementGroup } from './element';
+import React from 'react';
 
 export function Canvas() {
   const elementGroupAtoms = useAtomValue(elementGroupAtomsAtom);
   const setSelectedElementGroupAtoms = useSetAtom(selectedElementGroupAtomsAtom);
   const setActiveElementAtom = useSetAtom(activeElementAtomAtom);
+  const mousePosition = React.useRef({ x: 0, y: 0 });
 
-  const handleCanvasClick = () => {
-    console.log('handleCanvasClick');
-    setSelectedElementGroupAtoms([]);
-    setActiveElementAtom(null);
+  const handleCanvasMouseDown = (e: React.MouseEvent) => {
+    mousePosition.current = { x: e.clientX, y: e.clientY };
+  };
+
+  const handleCanvasMouseUp = (e: React.MouseEvent) => {
+    // if the e.clientX and e.clientY are the same as the mousePosition.current
+    // then the user clicked on the canvas
+    // else the user dragged the mouse
+    if (mousePosition.current.x === e.clientX && mousePosition.current.y === e.clientY) {
+      setSelectedElementGroupAtoms([]);
+      setActiveElementAtom(null);
+    }
   };
 
   return (
     <Box
       id="canvas"
-      onClick={handleCanvasClick}
+      onMouseDown={handleCanvasMouseDown}
+      onMouseUp={handleCanvasMouseUp}
       sx={(theme) => ({
         width: 900,
         height: 700,
